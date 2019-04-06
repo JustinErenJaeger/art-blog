2b7aec6c1b812c6a630f197fb5af7b5d4028a473
JustinErenJaeger <soulcali789@gmail.com>
2019-04-06 09:23:27 +0200

    fixed start error

--- app.js
+++ app.js
@@ -51,5 +51,5 @@ app.use(function(err, req, res, next) {
 
 // module.exports = app;
 app.listen(8080);
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, console.log(`Server running on port ${PORT}..`));
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, console.log(`Server running on port ${PORT}..`));
